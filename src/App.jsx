3d47f91aa1dd0409ce675c7fdf9825a16dddc19a
TiagoM13 <thyagomotha2000@gmail.com>
2021-12-14 18:56:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from './components/RepositoryList';
+
+import './styles/global.scss';
+
+export function App() {
+    return (
+        <>
+            <RepositoryList />
+        </>
+    )
+}
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+export function RepositoryList() {
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/TiagoM13/repos')
+        //fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+            {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}   
+            </ul>
+        </section>
+    );
+}

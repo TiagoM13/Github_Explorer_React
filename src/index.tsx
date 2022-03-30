cc6ed7453ff6adf80af9e864c4c2fa4233285854
Tiago M <thyagomotha2000@gmail.com>
2022-03-30 19:39:53 -0300

    update components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { render } from 'react-dom';
 import { App } from './App';
 
-
-render(<App />,  document.getElementById('root'))
+render(
+    <App />,  
+    document.getElementById('root')
+);
